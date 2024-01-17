aa121917f47228735a139274d209b8eeec403113
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-01-17 20:51:55 +0300

    Update Task_02.cs

--- Task_02.cs
+++ Task_02.cs
@@ -3,19 +3,19 @@ class Program
 {
     static void Main()
   {
-     Console.Write("Введите координаты точки X и Y через пробел:");
+     Console.Write("Enter the coordinates of the point:");
      string[] coordinates = Console.ReadLine().Split(' ');
      int x = Convert.ToInt32(coordinates[0]);
      int y = Convert.ToInt32(coordinates[1]);
      if (x > 0 && y > 0)
-     Console.WriteLine("1");
+     Console.WriteLine("I Quarter plane!");
      else if (x < 0 && y > 0)
-     Console.WriteLine("2");
+     Console.WriteLine("II Quarter plane!");
      else if (x < 0 && y < 0)
-     Console.WriteLine("3");
+     Console.WriteLine("III Quarter plane!");
      else if (x > 0 && y < 0)
-     Console.WriteLine("4");
+     Console.WriteLine("IV Quarter plane!");
      else
-      Console.WriteLine("Точка находится на оси координат");
+      Console.WriteLine("The point is located on the coordinate axis: ");
   }
 }
