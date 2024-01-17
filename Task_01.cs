4b78be734a6eb5ef7f1f7533f96d4b58ade8c927
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-01-17 20:12:34 +0300

    Update and rename Sem_2.sln to Task_01.cs

---
+++ Task_01.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number=Convert.ToInt32(Console.ReadLine());
+
+        if(number%7==0 && number%23==0)
+        {
+           Console.WriteLine("YES!");
+        }else
+        {
+            Console.WriteLine("NO!");
+        }
+    }
+}
